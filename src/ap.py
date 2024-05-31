f0d4b02ebfb2e380ca59ceddd4ba3e19aab35bcf
Alex Rad <37549748+lts-rad@users.noreply.github.com>
2024-05-31 11:28:10 -0700

    Update ap.py with lower rates

--- src/ap.py
+++ src/ap.py
@@ -84,7 +84,7 @@ def make_beacon_ies(ssid_name, channel):
   power = b"\x17"
   country = Dot11Elt(ID='Country', info=(b"US" + env + first_channel + num_channel + power))
 
-  rates = [12, 18, 24, 36, 48, 72, 96, 108]
+  rates = [0x82, 0x84, 0x0b, 0x16]
   AP_RATES = bytes(rates)
 
   BEACON_IES = Dot11Elt(ID="SSID", info=ssid_name) \
